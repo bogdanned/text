704b527cca687349223a5f26748852d349d4e736
bogdanned <bogdan.nedelcu@lokallead.de>
2019-03-17 12:41:28 +0100

    camera input

--- src/cameraInput.js
+++ src/cameraInput.js
@@ -3,6 +3,6 @@ import Webcam from "react-webcam";
 
 export default class Component extends React.Component {
   render() {
-  return <Webcam style={{width: "100%",height: "129px"}}/>;
+  return <Webcam style={{width: "100%",height: "140px"}}/>;
   }
 }
